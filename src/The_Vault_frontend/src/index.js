6775fec3fc6cce228409b79fe113e62e3026ce5f
Niamh <niamhspingies@gmail.com>
2022-12-01 20:50:27 +0200

    Initial commit.

---
+++ src/The_Vault_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { The_Vault_backend } from "../../declarations/The_Vault_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await The_Vault_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
